af7afe88e1087db6ff55ebdd5f356860d9837509
Tarun Singhal <tarun.singhal224@gmail.com>
2021-05-01 12:10:04 +0530

    Third one

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.post("/", function(req, res){
   // const firstName = req.body.fName;
   // const lastName = req.body.lName;
   // const emailId = req.body.emailId;
-  //
+
   // const data = {
   //   members: [
   //     {
@@ -33,26 +33,26 @@ app.post("/", function(req, res){
   // };
   // const jsonData = JSON.stringify(data);
   // const url = "https://us7.api.mailchimp.com/3.0/lists/4297802829";
-  //
+
   // const options = {
   //   method: "POST",
   //   auth: "tarun0:d64c2c125bd689b61cdbbab052a4ad20-us7"
   // };
-  //
-  // const request = https.request(url, options, function(response){
-  //
-  //   if(response.statusCode === 200){
-  //     res.sendFile(__dirname + "/success.html");
-  //   }
-  //   else{
-  //     res.sendFile(__dirname + "/failure.html");
-  //   }
-  //
+
+  const request = https.request(url, options, function(response){
+
+    // if(response.statusCode === 200){
+    //   res.sendFile(__dirname + "/success.html");
+    // }
+    // else{
+    //   res.sendFile(__dirname + "/failure.html");
+    // }
+
   //   response.on("data", function(data){
   //     console.log(JSON.parse(data));
   //   });
   // });
-
+  //
   // request.write(jsonData);
   request.end();
 
